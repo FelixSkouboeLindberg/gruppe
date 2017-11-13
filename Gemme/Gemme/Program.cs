a6e0b7d8b6aff632a9a84776e43c07f5c3bddd33
Danny Haslund <dannyhasl1@gmail.com>
2017-11-13 10:45:28 +0100

    Dimonds

--- Gemme/Gemme/Program.cs
+++ Gemme/Gemme/Program.cs
@@ -11,12 +11,37 @@ namespace Gemme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Print("Do u like mining dimonds???");
+
+
+            Print("Vil du gerne mine dimonds??");
+            string valg = Console.ReadLine().ToLower();
+            Print("Hvor mange blocks, vil du gerne mine ned??");
+            int blocks = int.Parse(Console.ReadLine());
+
+            if(valg == "ja")
+            {
+                Mine(blocks);
+            }
+            
+            
             Console.ReadKey();
         }
         public static void Print(string text)
         {
             Console.WriteLine(text);
+        }
+        public static void Mine(int blocks)
+        {
+            if(blocks < 100)
+            {
+                Console.WriteLine("Du har valgt at mine " + blocks + " ned");
+                Console.WriteLine("Du fandt 1000 dimonds");
+            } else
+            {
+                Console.WriteLine("Nå");
+                Console.WriteLine("Så langt måtte du ikke mine");
+            }
+            
         }
     }
 }
